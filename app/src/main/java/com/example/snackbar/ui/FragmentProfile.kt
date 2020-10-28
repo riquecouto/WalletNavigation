c30349bc8ac53131e288ab561e3715a1614c78d3
Henrique <henriquecouto@poli.ufrj.br>
2020-10-28 11:34:01 -0300

    Commit inicial

---
+++ app/src/main/java/com/example/snackbar/ui/FragmentProfile.kt
@@ -0,0 +1,28 @@
+package com.example.snackbar.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.snackbar.R
+import com.example.snackbar.R.id.ajudaFragment
+import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.fragment_profile.view.*
+
+class FragmentProfile : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        val view: View = inflater!!.inflate(R.layout.fragment_profile, container, false)
+        view.button.setOnClickListener(){
+            findNavController().navigate(R.id.action_fragmentProfile_to_ajudaFragment)
+        }
+        return view
+    }
+}
